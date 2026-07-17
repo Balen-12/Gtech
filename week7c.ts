a7dfa519f3d925bf49755b5db6b459acb1c3ab77
Balen <balensalam88@gmail.com>
2026-07-18 00:03:04 +0300

    deletting medicine

---
+++ week7c.ts
@@ -0,0 +1,54 @@
+class Medicine {
+    name: string;
+    price:number;
+    quantity:number;
+    expierd:boolean;
+
+
+    constructor(name: string, price: number, quantity: number, expierd:boolean){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.expierd = expierd;
+    }
+}
+
+class Recipet {
+    name: string;
+    amount: number;
+    total: number;
+
+
+    constructor(name: string, amount: number, total: number){
+
+        this.name = name;
+        this.amount = amount;
+        this.total = total; 
+
+    }
+}
+
+class pharmacy {
+    private medicines: Medicine [] = [];
+    private salesProductsHistory: Recipet [] = [];
+
+
+    add(medicine: Medicine):void{
+
+        this.medicines.push(medicine);
+        console.log(`added ${this.medicines[this.medicines.length-1].name}`)
+
+    }
+
+deletMedicine(name: string): void{
+ for(let i = 0; i < this.medicines.length; i++){
+    if(this.medicines[i].name.toLowerCase() === name.toLowerCase()){
+this.medicines.splice(i,1);
+console.log(`medicine deleted: ${name}`);
+return;
+    }
+    
+ }
+ console.log(`medicine not found`);
+}
+}
