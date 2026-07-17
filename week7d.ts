da5bdc9f5d79daf41dd26bc44bb7f52057305ee1
Balen <balensalam88@gmail.com>
2026-07-18 00:06:44 +0300

    medicines update

---
+++ week7d.ts
@@ -0,0 +1,65 @@
+class Medicine {
+    name: string;
+    price:number;
+    quantity:number;
+    expierd:boolean;
+
+
+    constructor(name: string, price: number, quantity: number, expierd:boolean){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.expierd = expierd;
+    }
+}
+
+class Recipet {
+    name: string;
+    amount: number;
+    total: number;
+
+
+    constructor(name: string, amount: number, total: number){
+
+        this.name = name;
+        this.amount = amount;
+        this.total = total; 
+
+    }
+}
+
+class pharmacy {
+    private medicines: Medicine [] = [];
+    private salesProductsHistory: Recipet [] = [];
+
+
+    add(medicine: Medicine):void{
+
+        this.medicines.push(medicine);
+        console.log(`added ${this.medicines[this.medicines.length-1].name}`)
+
+    }
+
+    search(name: string): Medicine | null {
+for(let i = 0; i < this.medicines.length; i++){
+    if(this.medicines[i].name.toLowerCase().includes(name.toLowerCase())){
+        return this.medicines[i];
+    }
+
+}
+ return null;
+    }
+
+
+updagtePrice(name: string , newPrice:number): void{
+    let found = this.search(name);
+    if(found){
+        found.price = newPrice;
+        console.log(`price updated for ${name}: ${newPrice} IQD`)
+    }else{
+
+        console.log(`medicine not found`)
+    }
+
+}
+}
