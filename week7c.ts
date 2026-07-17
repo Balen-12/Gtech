25f3125fdc1f3e2265291893fdc00571fc1d1786
Balen <balensalam88@gmail.com>
2026-07-18 01:07:19 +0300

    deleting medicines

--- week7c.ts
+++ week7c.ts
@@ -52,3 +52,10 @@ return;
  console.log(`medicine not found`);
 }
 }
+
+
+const store = new pharmacy();
+store.add(new Medicine("dogalaz", 2000, 15, true));
+store.add(new Medicine("Paracetamol", 1000, 20, false));
+store.add(new Medicine("Ibuprofen", 1500, 8, true));
+store.deletMedicine(`dogalaz`);
