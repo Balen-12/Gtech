14e0b85ec7d62fc32ff5208487035d8ea496dde0
Balen <balensalam88@gmail.com>
2026-07-18 01:13:41 +0300

    restock

--- week7g.ts
+++ week7g.ts
@@ -64,3 +64,10 @@ Resotck(): void {
 }
 
 }
+
+
+const store = new pharmacy();
+store.add(new Medicine("dogalaz", 2000, 15, true));
+store.add(new Medicine("Paracetamol", 1000, 20, false));
+store.add(new Medicine("Ibuprofen", 1500, 8, true));
+store.Resotck();
